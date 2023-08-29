58674d1a29ad068607a537f3625c4a210e4802ee
nohranferreira <115414025+nohranferreira@users.noreply.github.com>
2023-08-29 17:49:41 -0300

    Add files via upload

---
+++ assets/js/carrossel.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+	wrapper: true,
+	speed: 1000,
+	spaceBetween: 20,
+	slidesPerView: 2,
+	autoplay: {
+		delay: 5000,
+	},
+	loop: true,
+    parallax: true,
+});
